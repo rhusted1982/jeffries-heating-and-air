0914e6fb23af9288959838d5e4a1b40ca731b686
Richard Husted Jr <richard.husted@gmail.com>
2016-07-30 18:12:32 -0500

    Set up initial development environment.

---
+++ tools/srcServer.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import webpack from 'webpack';
+import path from 'path';
+import open from 'open';
+import config from '../webpack.config.dev';
+
+/*eslint-disable no-console */
+
+const port = 9682;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
+
+app.get('*', function(request, response) {
+    response.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(error) {
+    if(error) {
+        console.log(error);
+    } else {
+        open(`http://localhost:${port}`);
+    }
+});
